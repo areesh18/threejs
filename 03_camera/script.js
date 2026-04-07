cc06709db4845fbdffe9fdff1db3b50061297393
Areesh18 <areeshzafar396@gmail.com>
2026-04-07 23:30:25 +0530

    seeing top and bottom of cube -animating position.y

--- 03_camera/script.js
+++ 03_camera/script.js
@@ -58,6 +58,7 @@ const clock = new THREE.Clock();
 const tick = () => {
   camera.position.x = Math.sin(cursor.x*Math.PI*2)*3;
   camera.position.z = Math.cos(cursor.x*Math.PI*2)*3;
+  camera.position.y=cursor.y*5;
   camera.lookAt(mesh.position);
 
   renderer.render(scene, camera);
